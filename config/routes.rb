f5fe59301177391188136ba7c02f8eaa83957b4f
Hailin Li <hailin.li66@live.com>
2012-03-13 20:20:05 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp2::Application.routes.draw do
-  get "users/new"
+  root to: 'static_pages#home'
 
-  get "static_pages/home"
+  match '/signup', to: 'users#new'
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/about"
+  match '/about', to: 'static_pages#about'
+
+  match '/home', to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
